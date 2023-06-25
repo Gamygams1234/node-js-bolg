7533123bcf9301c444b1d24dd0c9326a8fb3bac4
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-06-24 18:55:00 -0700

    setting up header and footer with ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// Routes
+// Routes render the ejs files depending on the situation
 router.get("" , (req, res)=>{
-    res.render("index")
+    const locals = {
+        title: "Node JS Blog",
+        description: "Simple Blog created with NodeJS, Express, and MongoDB"
+
+    }
+    res.render("index", {locals})
+})
+router.get("/about" , (req, res)=>{
+    res.render("about")
 })
 
 // this is important to export the router
